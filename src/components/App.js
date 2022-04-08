823976570d688bcd6a205d54f3fe70de3862e783
harish <harishvarmaa31@gmail.com>
2022-04-08 22:41:28 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import { Preview } from './Preview';
 const App = () => {
 
 
-const[content,setContent]=useState("hello world")
+const[content,setContent]=useState("hello newton")
 const[fontSize,setFontSize]=useState(10)
 const [padding,setPadding]=useState(5)
 
@@ -12,9 +12,9 @@ const [padding,setPadding]=useState(5)
   return (
     <div id="main">
       <div>
-<input  type="text"  id="contentInput" value={content} onChange={(e)=>setContent(e.target.value)} />
-<input type="number" id="fontSizeInput" value={fontSize} onChange={(e)=>setFontSize(e.target.value)} />
-<input type="number" id="paddingInput" value={padding} onChange={(e)=>setPadding(e.target.value)}  />
+<input  type='text'  id="contentInput" value={content} onChange={(e)=>setContent(e.target.value)} />
+<input type='number' id="fontSizeInput" value={fontSize} onChange={(e)=>setFontSize(e.target.value)} />
+<input type='number' id="paddingInput" value={padding} onChange={(e)=>setPadding(e.target.value)}  />
 
         {/* add input elememnts inside here */}
       </div>
